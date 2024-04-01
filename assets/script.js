03530f1e0b940e28672ac9afe0bdd6da10ee296e
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-04-01 17:25:09 +0200

    ajouts  eventListener

--- assets/script.js
+++ assets/script.js
@@ -18,19 +18,18 @@ const slides = [
 ]
 
 
-// Sélectionner les flèches gauche et droite
-let arrowLeft = document.getElementById('arrow_left'); // Sélectionne la flèche gauche
-let arrowRight = document.getElementById('arrow_right'); // Sélectionne la flèche droite
+const left = document.querySelector(".arrow_left");
+console.log(left);
+const right = document.querySelector(".arrow_right");
+console.log(right);
 
-// Ajouter des écouteurs d'événements sur les flèches gauche et droite
-arrowLeft.addEventListener('click', function() {
-    // Logique pour naviguer vers la gauche dans le carrousel
-    console.log('Clic sur la flèche gauche'); // Affiche un message dans la console indiquant que la flèche gauche a été cliquée
-    alert('Clic sur la flèche gauche'); // Affiche une alerte indiquant que la flèche gauche a été cliquée
-});
+left.addEventListener("click", showLeftSlide);
+right.addEventListener("click", showRightSlide);
 
-arrowRight.addEventListener('click', function() {
-    // Logique pour naviguer vers la droite dans le carrousel
-    console.log('Clic sur la flèche droite'); // Affiche un message dans la console indiquant que la flèche droite a été cliquée
-    alert('Clic sur la flèche droite'); // Affiche une alerte indiquant que la flèche droite a été cliquée
-});
+function showLeftSlide() {
+    console.log("Tu as cliqué sur la flèche gauche");
+}
+
+function showRightSlide() {
+    console.log("Tu as cliqué sur la flèche droite");
+}
