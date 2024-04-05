598c6787ef4f41f2dc9b7069c23f750f98a3eea3
Nicolas Guapo <nicolas.lecoeur.guapo@gmail.com>
2024-04-05 14:24:18 +0200

    finalisation du projet

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ const dots = document.querySelectorAll(".dot");
 const bannerSlides = document.querySelector(".banner-img");
 const bannerText = document.querySelector("p");
 
+// Définition des différentes diapositives du carrousel avec leurs images et lignes de légende associées
 
 const slides = [
 	{
@@ -27,13 +28,27 @@ const slides = [
 ]
 
 // Ajout des écouteurs d'événements
+
+function showPreviousSlide() {
+    console.log("Clic sur la flèche gauche");
+}
+
+function showNextSlide() {
+    console.log("Clic sur la flèche droite");
+}
+
 left.addEventListener("click", showPreviousSlide);
 right.addEventListener("click", showNextSlide);
 
+// Initialisation de l'index de la diapositive actuelle à zéro
 let currentSlide = 0;
+
+// Appel de la fonction pour mettre à jour les points du carrousel
 updateActiveDot();
 
+// Fonction pour mettre à jour les points du carrousel
 function updateActiveDot () {
+    // Boucle pour parcourir tous les points du carrousel
     dots.forEach ((dot, index) => {
         dot.classList.remove("dot_selected");
         if (index === currentSlide) {
@@ -49,11 +64,11 @@ function updateActiveDot () {
 
 function showNextSlide() {
     console.log("Clic sur la flèche droite"); 
-    currentSlide = (currentSlide + 1) % slides.length;
+    currentSlide = (currentSlide + 1) % slides.length;// Utilisation de l'opérateur modulo (%) pour assurer un défilement circulaire et donc infini des diapositives
     bannerSlides.src = "./assets/images/slideshow/" + slides[currentSlide].image;
 	console.log
     bannerText.innerHTML = slides[currentSlide].tagLine;
-    updateActiveDot();
+    
 }
 
 function showPreviousSlide () {
